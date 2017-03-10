f67d7cd02164c949354c1cc37dffd8858afc55d5
Alexandre M. Assis <alexandre.assis@ca.com>
2017-03-10 20:43:02 -0300

    v5.3 Update

---
+++ DataLoad/SvcAppRead.bat
@@ -0,0 +1,13 @@
+@Echo Off
+SET XOG_HOME=c:\apps\clients\xog151\bin
+SET SERVERNAME=ppmlatam174.ca.com
+SET PORTNUMBER=80
+SET SSLENABLED=false
+SET USERNAME=xog
+SET PASSWORD=gox
+SET JAVA_HOME=c:/apps/jdk1.8.0_66
+IF NOT EXIST output md output
+CD C:\Users\assal01\Documents\GitHub\Strategic-Alignment\DataLoad
+Call %XOG_HOME%/xog -servername %SERVERNAME% -portnumber %PORTNUMBER% -sslenabled %SSLENABLED%  -username %USERNAME% -password %PASSWORD% -output c:\Users\assal01\Documents\GitHub\Strategic-Alignment\DataLoad/output/ApplicationRead_out.xml -input c:\Users\assal01\Documents\GitHub\Strategic-Alignment\DataLoad/ApplicationRead.xml
+Call %XOG_HOME%/xog -servername %SERVERNAME% -portnumber %PORTNUMBER% -sslenabled %SSLENABLED%  -username %USERNAME% -password %PASSWORD% -output c:\Users\assal01\Documents\GitHub\Strategic-Alignment\DataLoad/output/ServiceRead_out.xml -input c:\Users\assal01\Documents\GitHub\Strategic-Alignment\DataLoad/ServiceRead.xml
+pause
